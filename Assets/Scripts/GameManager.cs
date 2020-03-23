1fe541f32efe134a4d2eae6672340576a6bbf1a7
kudoranze <62541162+kudoranze@users.noreply.github.com>
2020-03-23 20:38:49 +0900

    2020323_2038
    new

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	//カメラを格納する変数
+	public Camera Camera;
+	public Camera SubCamera;
+
+	//キャンパスを格納
+	public GameObject canvas;
+
+	void Start()
+	{
+		//初めはカメラをオフにしとく
+		SubCamera.enabled = false;
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	
+	//ボタンを押した時の処理
+	public void PushButton ()
+	{
+		//サブカメラがオフだったら
+		if (!SubCamera.enabled)
+		{
+			//サブカメラをオンにして
+			SubCamera.enabled = true;
+
+			//カメラをオフにする
+			Camera.enabled = false;
+
+			//キャンパスを映すカメラをサブカメラオブジェクトにする
+			GetComponent<Canvas>().worldCamera = SubCamera;
+		}
+		//サブカメラがオンだったら
+		else
+		{
+			//サブカメラをオフにして
+			SubCamera.enabled = false;
+
+			//カメラをオンにする
+			Camera.enabled = true;
+
+			//キャンパスを映すカメラをカメラオブジェクトにする
+			GetComponent<Canvas>().worldCamera = Camera;
+		}
+		
+	}
+}
