c5f59a0b19f2da7e6abae6885b0089ca9f43a1f4
LightToRingo <lightblooooo@gmail.com>
2020-03-23 19:07:20 +0900

    Added LineSimulator.cs
    LineSimulatorの実装(CueBall物理予測線)

---
+++ Assets/Scripts/LineSimulator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class LineSimulator : MonoBehaviour
+{
+    public GameObject cueBall;
+    //lineRendererComponent
+    public GameObject line;
+    //カメラの現在位置
+    private Vector3 currentCameraPos;
+    private LineRenderer lineRenderer;
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        Vector3 pos = transform.position;
+
+        //物理運動軌道予測線に必要な変数 
+        lineRenderer = line.GetComponent<LineRenderer>();
+        currentCameraPos = Camera.main.transform.position;
+        currentCameraPos -= pos;
+        currentCameraPos *= 1 * -1;
+        currentCameraPos.y = 0;
+
+        //物理運動軌道予測線 描写
+        lineRenderer.SetPosition(0, cueBall.transform.position);
+        lineRenderer.SetPosition(1, currentCameraPos);
+
+        //lineの太さ
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+    }
+}
